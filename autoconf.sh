a8b12454d523dc337cb42858e5abec115c83e7da
AlexBrightwater <alexander.brightwater@gmail.com>
2022-06-20 20:52:47 +0200

    Add restore function

--- autoconf.sh
+++ autoconf.sh
@@ -65,8 +65,30 @@ echo "help page"
 
 }
 
+function restore_promt () {
+###############################
+# Restore from latest backup  #
+###############################
 
+read -p "Do you really want to continiue with the restore from backup? (y/n) " choice
+case "$choice" in 
+	y|Y ) echo "Continuing with the restore process"; restore_do;;
+	n|N ) echo "Aborting the restore process"; exit 0;;
+  	* ) echo "The answer was invalid. Please try again"; restore_promt ;;
+		
+esac
+}
 
+function restore_do () {
+latest=$(ls -td $conf_path/.BAK/* | head -1)
+cp $latest/.zshrc ~/.zshrc
+cp $latest/.bash_aliases ~/.bash_aliases
+cp $latest/.p10k.zsh ~/.p10k.zsh
+cp $latest/.vimrc ~/.vimrc
+
+echo "Restore complete"
+exit 0
+}
 
 ###############################################
 # get options and run the specified functions #
